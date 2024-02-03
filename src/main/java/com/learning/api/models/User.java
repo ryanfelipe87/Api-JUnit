2113574e12bceb86e86042e8f757c7646e2dcf4f
Ryan <ryanfelipe.rr@gmail.com>
2024-02-03 06:45:51 -0300

    User class

---
+++ src/main/java/com/learning/api/models/User.java
@@ -0,0 +1,22 @@
+package com.learning.api.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+    private String password;
+}
