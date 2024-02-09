23583c823f56a11724ba78d57139c7b8fc2f2e11
Ryan <ryanfelipe.rr@gmail.com>
2024-02-09 13:23:22 -0300

    Create Endpoint

--- src/main/java/com/learning/api/controllers/UserController.java
+++ src/main/java/com/learning/api/controllers/UserController.java
@@ -6,11 +6,10 @@ import com.learning.api.services.UserService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -34,5 +33,12 @@ public class UserController {
         List<UserDto> listDto = userService.findAll().stream().map(x -> mapper.map(x, UserDto.class))
                 .collect(Collectors.toList());
         return ResponseEntity.ok().body(listDto);
+    }
+
+    @PostMapping
+    public ResponseEntity<UserDto> create(@RequestBody UserDto userDto){
+        User user = userService.create(userDto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 }
--- src/main/java/com/learning/api/services/UserService.java
+++ src/main/java/com/learning/api/services/UserService.java
@@ -1,8 +1,10 @@
 package com.learning.api.services;
 
+import com.learning.api.dtos.UserDto;
 import com.learning.api.exceptions.ObjectNotFoundException;
 import com.learning.api.models.User;
 import com.learning.api.repositories.UserRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +17,9 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private ModelMapper mapper;
+
     public User findById(Long id) {
        return userRepository.findById(id)
                 .orElseThrow(() -> new ObjectNotFoundException("Object not found"));
@@ -22,5 +27,9 @@ public class UserService {
 
     public List<User> findAll(){
         return userRepository.findAll();
+    }
+
+    public User create(UserDto userDto){
+        return userRepository.save(mapper.map(userDto, User.class));
     }
 }
