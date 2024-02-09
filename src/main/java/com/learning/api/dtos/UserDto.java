ddcb2ac1ef677b9dac8c720a8c033f8c18d099ad
Ryan <ryanfelipe.rr@gmail.com>
2024-02-09 13:40:16 -0300

    Log console config and remove imports of the classes

--- src/main/java/com/learning/api/dtos/UserDto.java
+++ src/main/java/com/learning/api/dtos/UserDto.java
@@ -1,6 +1,5 @@
 package com.learning.api.dtos;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
--- src/main/java/com/learning/api/exceptions/ObjectNotFoundException.java
+++ src/main/java/com/learning/api/exceptions/ObjectNotFoundException.java
@@ -1,7 +1,5 @@
 package com.learning.api.exceptions;
 
-import java.util.function.Supplier;
-
 public class ObjectNotFoundException extends RuntimeException {
 
     public ObjectNotFoundException(String message){
