236a6627c8eafa2e3ecfba7054f15df0fe0ac0a2
Ryan <ryanfelipe.rr@gmail.com>
2024-02-09 12:42:04 -0300

    UserDto class created

---
+++ src/main/java/com/learning/api/dtos/UserDto.java
@@ -0,0 +1,18 @@
+package com.learning.api.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDto {
+
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+}
