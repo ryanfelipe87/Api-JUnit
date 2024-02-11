d058e30be15e87424791b2a69f541339497e645c
Ryan <ryanfelipe.rr@gmail.com>
2024-02-11 19:03:20 -0300

    Test for class UserService for findAll method

--- src/test/java/com/learning/api/services/UserServiceTest.java
+++ src/test/java/com/learning/api/services/UserServiceTest.java
@@ -8,10 +8,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.modelmapper.ModelMapper;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
@@ -25,6 +25,7 @@ class UserServiceTest {
     private static final String NAME = "Ryan";
     private static final String EMAIL = "rshow@example.com";
     private static final String PASSWORD = "7432";
+    public static final int INDEX = 0;
 
     @InjectMocks
     private UserService userService;
@@ -70,7 +71,18 @@ class UserServiceTest {
     }
 
     @Test
-    void findAll() {
+    void whenFindAllThenReturnAnListOfUsers() {
+        when(userRepository.findAll()).thenReturn(List.of(user));
+
+        List<User> response = userService.findAll();
+        assertNotNull(response);
+        assertEquals(1, response.size());
+        assertEquals(User.class, response.get(INDEX).getClass());
+
+        assertEquals(ID, response.get(INDEX).getId());
+        assertEquals(NAME, response.get(INDEX).getName());
+        assertEquals(EMAIL, response.get(INDEX).getEmail());
+        assertEquals(PASSWORD, response.get(INDEX).getPassword());
     }
 
     @Test
