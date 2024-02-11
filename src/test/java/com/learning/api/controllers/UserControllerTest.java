13b0020e8fa47862482917cca7e53c24d5e09e61
Ryan <ryanfelipe.rr@gmail.com>
2024-02-11 20:20:27 -0300

    Test for class UserController, initial configuration

---
+++ src/test/java/com/learning/api/controllers/UserControllerTest.java
@@ -0,0 +1,70 @@
+package com.learning.api.controllers;
+
+import com.learning.api.dtos.UserDto;
+import com.learning.api.models.User;
+import com.learning.api.services.UserService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class UserControllerTest {
+
+    private static final Long ID = 1L;
+    private static final String NAME = "Ryan";
+    private static final String EMAIL = "rshow@example.com";
+    private static final String PASSWORD = "7432";
+    public static final int INDEX = 0;
+
+    private User user;
+
+    private UserDto userDto;
+
+    @InjectMocks
+    private UserController userController;
+
+    @Mock
+    private UserService userService;
+
+    @Mock
+    private ModelMapper mapper;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        startUser();
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    private void startUser(){
+        user = new User(ID, NAME, EMAIL, PASSWORD);
+        userDto = new UserDto(ID, NAME, EMAIL, PASSWORD);
+    }
+}
