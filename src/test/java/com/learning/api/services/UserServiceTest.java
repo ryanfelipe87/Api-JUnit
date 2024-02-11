bdcaebf8da62ddf7c98f3658aa414ce9cd0a4837
Ryan <ryanfelipe.rr@gmail.com>
2024-02-11 19:25:13 -0300

    Test for class UserService for create successfully method

--- src/test/java/com/learning/api/services/UserServiceTest.java
+++ src/test/java/com/learning/api/services/UserServiceTest.java
@@ -16,6 +16,7 @@ import java.util.Optional;
 
 import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 
@@ -86,7 +87,17 @@ class UserServiceTest {
     }
 
     @Test
-    void create() {
+    void whenCreateThenReturnSuccess() {
+        when(userRepository.save(any())).thenReturn(user);
+
+        User response = userService.create(userDto);
+
+        assertNotNull(response);
+        assertEquals(User.class, response.getClass());
+        assertEquals(ID, response.getId());
+        assertEquals(NAME, response.getName());
+        assertEquals(EMAIL, response.getEmail());
+        assertEquals(PASSWORD, response.getPassword());
     }
 
     @Test
